c411b5a20a01ebb71e44d6bcac9aa56cdc80bea8
Wes Turner <wes@wrd.nu>
2026-04-27 07:32:21 +0000

    CLN: next.config.mjs: move typedRoutes outside of experimental block

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    typedRoutes: true,
-  },
+  typedRoutes: true,
+  //experimental: { },
 };
 
 export default nextConfig;
